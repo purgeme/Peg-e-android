e20620760e1df37f2d16c2bb1d54931d8479f92d
Parvesh Kumar <jostaboi@protonmail.com>
2021-06-17 00:19:03 +0530

    Modified to move to react navigation 5.x

--- app/screens/Navigator.js
+++ app/screens/Navigator.js
@@ -1,20 +1,45 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './HomeScreen';
 import GameScreen from './Game';
 import Success from './success';
 import Pause from './pause';
+import { NavigationContainer } from '@react-navigation/native';
 
-const HomeStack = createStackNavigator(
-  {
-    HomeScreen,
-    Pause,
-    GameScreen,
-    Success,
-  },
-  {
-    initialRouteName: 'HomeScreen',
-  }
+const Stack = createStackNavigator();
+
+const HomeStack = (
+  <Stack.Navigator
+    initialRouteName="HomeScreen"
+  >
+    <Stack.Screen
+      name="HomeScreen"
+      component={HomeScreen}
+    />
+    <Stack.Screen
+      name="GameScreen"
+      component={GameScreen}
+    />
+    <Stack.Screen
+      name="Pause"
+      component={Pause}
+    />
+    <Stack.Screen
+      name="Success"
+      component={Success}
+    />
+  </Stack.Navigator>
+  // {
+  //   HomeScreen,
+  //   Pause,
+  //   GameScreen,
+  //   Success,
+  // },
+  // {
+  //   initialRouteName: 'HomeScreen',
+  // }
 );
 
-export default createAppContainer(HomeStack);
+export default function navigator() {
+  return <NavigationContainer>{HomeStack}</NavigationContainer>
+};
